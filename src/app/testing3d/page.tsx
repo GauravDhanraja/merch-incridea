23629a18969842dc2539cf1e8fe276e2e127bdf0
Keerthan <kkeerthan2005@gmail.com>
2024-12-25 21:48:41 +0530

    TShirt Draft

--- src/app/testing3d/page.tsx
+++ src/app/testing3d/page.tsx
@@ -1,7 +1,9 @@
-"use client"
+"use client";
 import RenderModel from "~/app/testing3d/RenderModel";
 import { FridgeMagnet } from "~/app/testing3d/models/FridgeMagnet";
 import { atom } from "jotai";
+import { TShirt } from "~/app/testing3d/models/TShirt";
+import {KeyChain} from "~/app/testing3d/models/KeyChain";
 
 export const wiggleAtom = atom(true);
 
@@ -10,10 +12,10 @@ export default function Home() {
     <main className="relative flex min-h-screen flex-col items-center justify-between">
       {/*<Image src={bg} alt="background-image" fill*/}
       {/*       className="-z-50 w-full h-full object-cover object-center opacity-25"></Image>*/}
-      <div className="h-screen w-full">
+      <div className="h-screen w-screen">
         {/*navigation and 3D model*/}
         <RenderModel>
-          <FridgeMagnet />
+            <TShirt/>
         </RenderModel>
       </div>
     </main>
