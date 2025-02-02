2054466e08f07ce3a3f2040a5671e8c0192faab4
GauravDhanraja <gauravdhanraja88@proton.me>
2025-02-02 11:43:01 +0530

    styles: responsive for tablet screen

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -108,11 +108,11 @@ export default function Shop() {
   };
 
   return (
-    <main className="relative flex min-h-screen w-full flex-col items-center justify-center overflow-x-hidden bg-gradient-to-bl from-emerald-950 to-emerald-800 pt-24 md:overflow-y-hidden md:pt-20">
+    <main className="relative flex min-h-screen w-full flex-col items-center justify-center overflow-x-hidden bg-gradient-to-bl from-emerald-950 to-emerald-800 pt-24 lg:overflow-y-hidden lg:pt-20">
       {isLoading ? (
         <CircleLoader />
       ) : (
-        <div className="flex min-h-screen w-full flex-col items-center justify-start space-y-8 p-4 pt-28 md:justify-center md:pt-6">
+        <div className="flex min-h-screen w-full flex-col items-center justify-start space-y-8 p-4 pt-28 lg:justify-center lg:pt-6">
           <div className="flex flex-wrap justify-center gap-14">
             {/* Remaining merchandise */}
             <div className="flex flex-wrap justify-center gap-14 pb-16">
@@ -126,15 +126,15 @@ export default function Shop() {
                       }}
                       data-index={0}
                       onClick={() => setActiveCard(0)}
-                      className={`relative cursor-pointer rounded-2xl p-4 shadow-lg transition-all duration-300 md:rounded-3xl md:p-6 ${
+                      className={`relative cursor-pointer rounded-2xl p-4 shadow-lg transition-all duration-300 lg:rounded-3xl lg:p-6 ${
                         activeCard === 0
-                          ? "h-[400px] w-72 scale-105 bg-gradient-to-tr from-emerald-600 to-emerald-400 text-white md:h-[450px] md:w-80"
-                          : "h-[350px] w-64 scale-95 bg-gradient-to-tr from-emerald-700 to-emerald-500 text-gray-300 md:h-[400px] md:w-72"
+                          ? "h-[400px] w-72 scale-105 bg-gradient-to-tr from-emerald-600 to-emerald-400 text-white lg:h-[450px] lg:w-80"
+                          : "h-[350px] w-64 scale-95 bg-gradient-to-tr from-emerald-700 to-emerald-500 text-gray-300 lg:h-[400px] lg:w-72"
                       }`}
                     >
                       {activeCard === 0 && (
                         <button
-                          className="absolute left-3 top-8 p-0 text-white hover:text-gray-200 md:left-4 md:top-10"
+                          className="absolute left-3 top-8 p-0 text-white hover:text-gray-200 lg:left-4 lg:top-10"
                           onClick={(e) => {
                             e.stopPropagation();
                             handlePreviousCard();
@@ -145,7 +145,7 @@ export default function Shop() {
                       )}
                       {activeCard === 0 && (
                         <button
-                          className="absolute right-3 top-8 p-0 text-white hover:text-gray-200 md:right-4 md:top-10"
+                          className="absolute right-3 top-8 p-0 text-white hover:text-gray-200 lg:right-4 lg:top-10"
                           onClick={(e) => {
                             e.stopPropagation();
                             handleNextCard();
@@ -154,8 +154,8 @@ export default function Shop() {
                           <FaChevronRight size={24} />
                         </button>
                       )}
-                      <div className="absolute -top-12 left-1/2 -translate-x-1/2 transform overflow-visible md:-top-16">
-                        <div className="h-36 w-36 md:h-48 md:w-48">
+                      <div className="absolute -top-12 left-1/2 -translate-x-1/2 transform overflow-visible lg:-top-16">
+                        <div className="h-36 w-36 lg:h-48 lg:w-48">
                           <Image
                             src={item.image}
                             alt={item.name}
@@ -165,12 +165,12 @@ export default function Shop() {
                           />
                         </div>
                       </div>
-                      <div className="mt-24 text-center md:mt-28">
+                      <div className="mt-24 text-center lg:mt-28">
                         <h2
                           className={
                             activeCard === 0
-                              ? "text-xl font-extrabold text-palate_1/90 md:text-2xl"
-                              : "text-lg font-semibold text-palate_1/60 md:text-xl"
+                              ? "text-xl font-extrabold text-palate_1/90 lg:text-2xl"
+                              : "text-lg font-semibold text-palate_1/60 lg:text-xl"
                           }
                         >
                           {item.name}
@@ -178,8 +178,8 @@ export default function Shop() {
                         <p
                           className={
                             activeCard === 0
-                              ? "text-sm font-semibold text-palate_1/90 md:text-base"
-                              : "text-xs font-normal text-palate_1/60 md:text-sm"
+                              ? "text-sm font-semibold text-palate_1/90 lg:text-base"
+                              : "text-xs font-normal text-palate_1/60 lg:text-sm"
                           }
                         >
                           {item.description}
@@ -187,8 +187,8 @@ export default function Shop() {
                         <p
                           className={
                             activeCard === 0
-                              ? "text-lg font-extrabold text-palate_1/90 md:text-2xl"
-                              : "text-base font-medium text-palate_1/60 md:text-lg"
+                              ? "text-lg font-extrabold text-palate_1/90 lg:text-2xl"
+                              : "text-base font-medium text-palate_1/60 lg:text-lg"
                           }
                         >
                           ₹{item.discountPrice}
@@ -197,7 +197,7 @@ export default function Shop() {
                           <div className="mt-6">
                             {session?.user.role === "CLASS_REP" ? (
                               <button
-                                className="rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black md:px-8 md:py-3"
+                                className="rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black lg:px-8 lg:py-3"
                                 onClick={() => setShowBulkOrderForm(true)}
                               >
                                 Bulk Order
@@ -205,7 +205,7 @@ export default function Shop() {
                             ) : (
                               <button
                                 disabled
-                                className="cursor-not-allowed rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black md:px-8 md:py-3"
+                                className="cursor-not-allowed rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black lg:px-8 lg:py-3"
                               >
                                 Buy through CR
                               </button>
@@ -222,15 +222,15 @@ export default function Shop() {
                       if (el) cardRefs.current[index + 1] = el;
                     }}
                     onClick={() => setActiveCard(index + 1)}
-                    className={`relative cursor-pointer rounded-2xl p-4 shadow-lg transition-all duration-300 md:rounded-3xl md:p-6 ${
+                    className={`relative cursor-pointer rounded-2xl p-4 shadow-lg transition-all duration-300 lg:rounded-3xl lg:p-6 ${
                       activeCard === index + 1
-                        ? "h-[400px] w-72 scale-105 bg-gradient-to-tr from-emerald-700 to-emerald-500 text-white md:h-[450px] md:w-80"
-                        : "h-[350px] w-64 scale-95 bg-gradient-to-tr from-emerald-800 to-emerald-600 text-gray-300 md:h-[400px] md:w-72"
+                        ? "h-[400px] w-72 scale-105 bg-gradient-to-tr from-emerald-700 to-emerald-500 text-white lg:h-[450px] lg:w-80"
+                        : "h-[350px] w-64 scale-95 bg-gradient-to-tr from-emerald-800 to-emerald-600 text-gray-300 lg:h-[400px] lg:w-72"
                     }`}
                   >
                     {activeCard === index + 1 && (
                       <button
-                        className="absolute left-3 top-8 p-0 text-white hover:text-gray-200 md:left-4 md:top-10"
+                        className="absolute left-3 top-8 p-0 text-white hover:text-gray-200 lg:left-4 lg:top-10"
                         onClick={(e) => {
                           e.stopPropagation();
                           handlePreviousCard();
@@ -241,7 +241,7 @@ export default function Shop() {
                     )}
                     {activeCard === index + 1 && (
                       <button
-                        className="absolute right-3 top-8 p-0 text-white hover:text-gray-200 md:right-4 md:top-10"
+                        className="absolute right-3 top-8 p-0 text-white hover:text-gray-200 lg:right-4 lg:top-10"
                         onClick={(e) => {
                           e.stopPropagation();
                           handleNextCard();
@@ -250,8 +250,8 @@ export default function Shop() {
                         <FaChevronRight size={24} color="white" />
                       </button>
                     )}
-                    <div className="absolute -top-12 left-1/2 -translate-x-1/2 transform overflow-visible md:-top-16">
-                      <div className="h-36 w-36 md:h-48 md:w-48">
+                    <div className="absolute -top-12 left-1/2 -translate-x-1/2 transform overflow-visible lg:-top-16">
+                      <div className="h-36 w-36 lg:h-48 lg:w-48">
                         <Image
                           src={item.image}
                           alt={item.name}
@@ -261,12 +261,12 @@ export default function Shop() {
                         />
                       </div>
                     </div>
-                    <div className="mt-24 text-center md:mt-28">
+                    <div className="mt-24 text-center lg:mt-28">
                       <h2
                         className={
                           activeCard === index + 1
-                            ? "text-xl font-extrabold text-palate_1/90 md:text-2xl"
-                            : "text-lg font-semibold text-palate_1/60 md:text-xl"
+                            ? "text-xl font-extrabold text-palate_1/90 lg:text-2xl"
+                            : "text-lg font-semibold text-palate_1/60 lg:text-xl"
                         }
                       >
                         {item.name}
@@ -274,8 +274,8 @@ export default function Shop() {
                       <p
                         className={
                           activeCard === index + 1
-                            ? "text-sm font-semibold text-palate_1/90 md:text-base"
-                            : "text-xs font-normal text-palate_1/60 md:text-sm"
+                            ? "text-sm font-semibold text-palate_1/90 lg:text-base"
+                            : "text-xs font-normal text-palate_1/60 lg:text-sm"
                         }
                       >
                         {item.description}
@@ -283,8 +283,8 @@ export default function Shop() {
                       <p
                         className={
                           activeCard === index + 1
-                            ? "text-lg font-extrabold text-palate_1/90 md:text-2xl"
-                            : "text-base font-medium text-palate_1/60 md:text-lg"
+                            ? "text-lg font-extrabold text-palate_1/90 lg:text-2xl"
+                            : "text-base font-medium text-palate_1/60 lg:text-lg"
                         }
                       >
                         ₹{item.discountPrice}
@@ -295,7 +295,7 @@ export default function Shop() {
                         }`}
                       >
                         <button
-                          className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300 text-lg font-bold md:h-10 md:w-10 md:text-xl"
+                          className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300 text-lg font-bold lg:h-10 lg:w-10 lg:text-xl"
                           onClick={(e) => {
                             e.stopPropagation();
                             setMerchData((prev) =>
@@ -312,11 +312,11 @@ export default function Shop() {
                         >
                           <FaMinus className="text-gray-700" />
                         </button>
-                        <span className="px-3 text-lg font-medium text-white md:px-4 md:text-xl">
+                        <span className="px-3 text-lg font-medium text-white lg:px-4 lg:text-xl">
                           {item.count}
                         </span>
                         <button
-                          className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300 text-lg font-bold md:h-10 md:w-10 md:text-xl"
+                          className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300 text-lg font-bold lg:h-10 lg:w-10 lg:text-xl"
                           onClick={(e) => {
                             e.stopPropagation();
                             setMerchData((prev) =>
@@ -332,7 +332,7 @@ export default function Shop() {
                         </button>
                       </div>
                       {activeCard === index + 1 && (
-                        <div className="mt-4 flex flex-wrap justify-center gap-2 md:mt-6 md:gap-3">
+                        <div className="mt-4 flex flex-wrap justify-center gap-2 lg:mt-6 lg:gap-3">
                           <BuyButton
                             merch={[
                               {
@@ -342,11 +342,11 @@ export default function Shop() {
                               },
                             ]}
                             total={item.discountPrice * item.count}
-                            className="rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black md:px-8 md:py-3"
+                            className="rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black lg:px-8 lg:py-3"
                           />
                           {session?.user ? (
                             <button
-                              className="rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black md:px-8 md:py-3"
+                              className="rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black lg:px-8 lg:py-3"
                               onClick={async () => {
                                 await addItemToCart.mutateAsync({
                                   id: item.id,
@@ -358,7 +358,7 @@ export default function Shop() {
                               Add to Cart
                             </button>
                           ) : (
-                            <button className="cursor-not-allowed rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black md:px-8 md:py-3">
+                            <button className="cursor-not-allowed rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black lg:px-8 lg:py-3">
                               Login for Cart
                             </button>
                           )}
@@ -433,7 +433,7 @@ export default function Shop() {
                         size: size as Sizes,
                       }))}
                     total={bulkTotalCost}
-                    className="rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black md:px-8 md:py-3"
+                    className="rounded-full bg-white px-6 py-2 font-bold tracking-wide text-black lg:px-8 lg:py-3"
                   />
                 )}
               </div>
