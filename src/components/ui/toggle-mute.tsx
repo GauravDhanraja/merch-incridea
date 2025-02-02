6e493d7f666ba1c8036636e1a14e73787e63669e
Nandan Pai <nandanrp09@gmail.com>
2025-02-02 10:15:02 +0530

    fix: Minor fixes

--- src/components/ui/toggle-mute.tsx
+++ src/components/ui/toggle-mute.tsx
@@ -2,15 +2,21 @@
 import { GoUnmute, GoMute } from "react-icons/go";
 import { useMusic } from "~/components/ui/MusicContext";
 
-const ToggleMute = ({color} : {color: string}) => {
+const ToggleMute = ({ color }: { color: string }) => {
   const { isMusicPlaying, toggleMusic } = useMusic();
 
   return (
     <div>
-      {isMusicPlaying ? (
-        <GoMute className={`h-8 w-8 ${color} cursor-pointer`} onClick={toggleMusic} />
+      {!isMusicPlaying ? (
+        <GoMute
+          className={`h-8 w-8 ${color} cursor-pointer`}
+          onClick={toggleMusic}
+        />
       ) : (
-        <GoUnmute className={`h-8 w-8 ${color} cursor-pointer`} onClick={toggleMusic} />
+        <GoUnmute
+          className={`h-8 w-8 ${color} cursor-pointer`}
+          onClick={toggleMusic}
+        />
       )}
     </div>
   );
